7693e40bba252f16ffd5d332bdc1529a2a3122e4
Игорь <idmitrashko@ukr.net>
2020-12-02 19:59:42 +0200

    Added sound, implementated sound tree, jump,background Sound

--- Assets/Scripts/ControllerScene.cs
+++ Assets/Scripts/ControllerScene.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class ControllerScene : MonoBehaviour
 {
     public void LoadScene(int level) {
+        StartCoroutine(WaithLoad(level));        
+    }
+    public IEnumerator WaithLoad(int level) {
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene(level);
     }
+
 }
--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -6,14 +6,15 @@ using UnityEngine.Events;
 public class Tree : MonoBehaviour
 {
     [SerializeField] private UnityEvent levelComplete;
-
+    AudioSource AudioSource;
     private void Start() {
-
+        AudioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.tag == "Player")
         {
+            AudioSource.Play();
             levelComplete?.Invoke();
         }
     }
